c1ce07515ddfbb83ae18988ff502aff3c3a51155
Daniel Maximov <mr.quze@gmail.com>
2019-12-08 06:49:34 +0300

    fix(FlippableCard): dont spoiler the back side

--- src/components/FlippableCard.js
+++ src/components/FlippableCard.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import cn from "classnames";
 import styles from "./FlippableCard.module.css";
 
-const FlippableCard = ({ data, flipped, className, ...props }) => (
-  <div className={cn(styles.root, className)} {...props}>
-    <div className={cn(styles.inner, { [styles.flipped]: flipped })}>
-      <div className={styles.front}>{data.side1}</div>
-      <div className={styles.back}>{data.side2}</div>
+const FlippableCard = ({ data, flipped, className, ...props }) => {
+  const [backWord, setBackWord] = useState(data.side2);
+
+  useEffect(() => {
+    setTimeout(setBackWord, 600, data.side2);
+  }, [data.side2]);
+
+  return (
+    <div className={cn(styles.root, className)} {...props}>
+      <div className={cn(styles.inner, { [styles.flipped]: flipped })}>
+        <div className={styles.front}>{data.side1}</div>
+        <div className={styles.back}>{backWord}</div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default FlippableCard;
